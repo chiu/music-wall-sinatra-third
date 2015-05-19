971f877329068ecbc2c02eaaa59ecd43cb98042f
chiu <chiucode@gmail.com>
2015-05-19 03:35:10 +0000

    great

--- db/migrate/20150517235652_add_messages.rb
+++ db/migrate/20150517235652_add_messages.rb
@@ -5,8 +5,12 @@ class AddMessages < ActiveRecord::Migration
       t.string :author
       t.string :content
       t.string :url
-      
+      t.datetime :created_at
+      t.datetime :updated_at
+
 
     end
   end
 end
+
+
